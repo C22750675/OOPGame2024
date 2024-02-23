c632f75933c2c692e245a894321abf277f6b5608
Franz Josef Liquit <franz.l@Franzs-MacBook-Air.local>
2024-02-23 14:34:05 +0000

    Good luck

--- enemies/Mob.gd
+++ enemies/Mob.gd
@@ -10,8 +10,9 @@ var random_speed = 5
 # Acceleration of the mob in meters per second squared.
 @export var fall_acceleration = 75
 
-
-var health = 10
+var target_player = null
+var min_distance = 7
+var health = 50
 
 func _physics_process(delta):
 
@@ -30,6 +31,10 @@ func _physics_process(delta):
 	if new_position.distance_to(Vector3.ZERO) > 25:
 		
 		queue_free()
+		
+	find_nearest_player()
+	direction_management(direction)
+	
 
 # This function will be called from the Main scene.
 func initialize(start_position, player_position):
@@ -77,3 +82,51 @@ func take_damage(damage_amount):
 
 		# increment the number of mobs killed
 		GlobalVars.mobsKilled += 1
+
+func take_knockback(knockback_amount : float):
+	var knockback_direction = (target_player.global_transform.origin - global_transform.origin).normalized()
+	var knockback_distance = knockback_amount
+	
+	var knockback_vector = knockback_direction * knockback_distance
+	
+	# Move the character in the opposite direction of knockback
+	global_transform.origin += knockback_vector
+	move_and_slide()
+	
+func find_nearest_player():
+	# Retrieve all nodes tagged as enemies in the scene
+	var players = get_tree().get_nodes_in_group("player")
+	
+	# If there are no enemies, return early
+	if players.size() == 0:
+		target_player = null
+		return
+		
+	var closest_distance = float(min_distance)
+	var closest_player = null
+	
+
+	# Iterate through each enemy and calculate the distance to the characteraa
+	for player in players:
+		var distance_to_enemy = global_transform.origin.distance_to(player.global_transform.origin)
+	
+		# If the current enemy is closer than the previously closest one, update the closest enemy
+		if distance_to_enemy < closest_distance:
+			closest_distance = distance_to_enemy
+			closest_player = player
+			
+	# Update the target_enemy variable with the closest enemy found
+	target_player = closest_player
+	
+
+func direction_management(direction):
+	
+	var look_direction = direction
+
+	if look_direction == Vector3.ZERO and target_player != null:
+		look_direction = (target_player.global_transform.origin - global_transform.origin).normalized()
+
+	if look_direction != Vector3.ZERO:
+		var look_rotation = Basis().looking_at(look_direction, Vector3.UP)
+		$Pivot.global_transform.basis = look_rotation
+	
