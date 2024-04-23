27b68c7175ae1aa66d077da630972fc7e584b7a5
C22750675 <c22750675@mytudublin.ie>
2024-04-23 23:08:06 +0100

    Removed unused power up velocity code

--- powerUp/HealthPowerUp.gd
+++ powerUp/HealthPowerUp.gd
@@ -1,9 +1,5 @@
 extends CharacterBody3D
 
-
-# Acceleration of the mob in meters per second squared.
-@export var fallAcceleration = 500
-
 # Called when the node enters the scene tree for the first time
 func _ready():
 
@@ -11,11 +7,6 @@ func _ready():
 
 # Called every frame
 func _physics_process(delta):
-	
-	velocity = Vector3.ZERO
-
-	# The power up falls to the ground
-	velocity.y -= fallAcceleration * delta
 
 	# Rotate the power up
 	rotate_y(delta)
